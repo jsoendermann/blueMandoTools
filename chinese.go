505bdaa6e297e59a6dde7c72b66cc4a2784f3856
Jan Soendermann <json@riemann.local>
2013-04-15 20:17:29 +0100

    Initial commit

---
+++ chinese.go
@@ -0,0 +1,13 @@
+// Package chinese defines constants and other useful stuff that other
+// chinese-related packages rely on.
+package chinese
+
+
+type CharSet int
+
+const (
+    Trad = iota
+    Simp
+)
+
+
