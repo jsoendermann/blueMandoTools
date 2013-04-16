1c4b126fb2366113a03a8a6ca633d32c1a9cda03
Yang Chuanzhang <json@riemann.local>
2013-04-16 19:51:08 +0100

    Comment out ToHTMLer interface

--- chinese.go
+++ chinese.go
@@ -13,9 +13,13 @@ const (
 // Different types of dictionary records can implement this interface
 // to provide a way to convert the data they hold to html for use on
 // a card. The colors must be in the form "#FFFFFF"
-type ToHTMLer interface {
-  ToHTML(toneColors []string) string
-}
+// TODO This is commented out because in its current form, this interface
+// makes no sense. The differences between dictionaries and between the
+// desired html output depending on the use case are too big to unify them
+// like this.
+//type ToHTMLer interface {
+//  ToHTML(toneColors []string) string
+//}
 
 // TODO Maybe add a ToCarder interface that converts records to 
 // csv rows.
