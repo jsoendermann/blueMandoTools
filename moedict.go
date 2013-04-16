8d4d21e414a45afcdb2f20216d600ebe53c5162e
Yang Chuanzhang <json@riemann.local>
2013-04-16 15:47:26 +0100

    Run gofmt

--- moedict.go
+++ moedict.go
@@ -3,7 +3,7 @@ package main
 //package moedict
 
 import (
-//	"fmt"
+	//	"fmt"
 	// "github.com/yangchuanzhang/chinese"
 	"encoding/json"
 	"io/ioutil"
@@ -14,54 +14,54 @@ import (
 // which is documented (in Chinese) here: https://hackpad.com/3du.tw-API-Design-95jKjray8uR
 // Fields that are commented out are not used at the moment.
 type Record struct {
-  Title string
-//	Radical                  string
-//	Stroke_count             int
-//	Non_radical_stroke_count int
+	Title string
+	//	Radical                  string
+	//	Stroke_count             int
+	//	Non_radical_stroke_count int
 
 	Heteronyms []Heteronym
 }
 
 type Heteronym struct {
-	Pinyin    string
-	Bopomofo  string
-//	Bopomofo2 string
+	Pinyin   string
+	Bopomofo string
+	//	Bopomofo2 string
 
 	Definitions []Definition
 }
 
 type Definition struct {
-	Def      string
-	Quote    []string
-	Example  []string
-//	DefType  string `json:"type"` // this field is called "type" in the output of the server
-//	Link     string
+	Def     string
+	Quote   []string
+	Example []string
+	//	DefType  string `json:"type"` // this field is called "type" in the output of the server
+	//	Link     string
 	Synonyms string
 }
 
 func FindRecord(word string) (*Record, error) {
-  // make http request, check for errors and defer close
+	// make http request, check for errors and defer close
 	resp, err := http.Get("http://www.moedict.tw/uni/" + word)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
-  // read data into variable
+	// read data into variable
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
-  // unmarshal json into r
-  var r Record
+	// unmarshal json into r
+	var r Record
 
-  jsonErr := json.Unmarshal(body, &r)
-  if jsonErr != nil {
+	jsonErr := json.Unmarshal(body, &r)
+	if jsonErr != nil {
 		return nil, jsonErr
 	}
 
-  return &r, nil
+	return &r, nil
 }
 
 func main() {
