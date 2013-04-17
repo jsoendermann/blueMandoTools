cd0476439d998ba24813384717099d1c306488b2
Yang Chuanzhang <json@riemann.local>
2013-04-17 11:51:45 +0100

    Add sentences.coffee file and update startServer.sh

--- startServer.sh
+++ startServer.sh
@@ -2,7 +2,12 @@
 
 rm -f vocab.html
 haml vocab.haml > vocab.html
+
 rm -f assets/vocab.js 
 coffee -c assets/vocab.coffee 
+
+rm -f assets/sentences.js
+coffee -c assets/sentences.coffee
+
 go install 
 blueServer 
