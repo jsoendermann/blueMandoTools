0ca2718db618f6afc993d55145945e7b7873166b
Yang Chuanzhang <json@riemann.local>
2013-04-18 14:22:47 +0100

    Move getRequestDataAndColors to util file

--- blueServer.go
+++ blueServer.go
@@ -218,18 +218,3 @@ func sentencesLookupHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(j))
 }
 
-// getRequestDataAndColors gets the word or sentence from the url
-// and the color array from the post data in the request
-func getRequestDataAndColors(r *http.Request) (string, []string) {
-	// get the word or sentence from the path
-	pathElements := strings.Split(r.URL.Path, "/")
-	requestData := pathElements[len(pathElements)-1]
-
-	// get colors
-	colors := make([]string, 5)
-	for i := 0; i <= 4; i++ {
-		colors[i] = r.FormValue(fmt.Sprintf("tone%d", i))
-	}
-
-	return requestData, colors
-}
--- blueServer_util.go
+++ blueServer_util.go
@@ -4,6 +4,8 @@ import (
 	"io/ioutil"
 	"strings"
   "regexp"
+  "net/http"
+  "fmt"
 )
 
 // This is a helper function that loads a text file into a string it returns
@@ -76,4 +78,20 @@ func includeFiles(htmlBeforeIncludes string) string {
 		return includeFiles(output)
 	}
 	return output
+}
+
+// getRequestDataAndColors gets the word or sentence from the url
+// and the color array from the post data in the request
+func getRequestDataAndColors(r *http.Request) (string, []string) {
+	// get the word or sentence from the path
+	pathElements := strings.Split(r.URL.Path, "/")
+	requestData := pathElements[len(pathElements)-1]
+
+	// get colors
+	colors := make([]string, 5)
+	for i := 0; i <= 4; i++ {
+		colors[i] = r.FormValue(fmt.Sprintf("tone%d", i))
+	}
+
+	return requestData, colors
 }
