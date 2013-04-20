4c2fda66329e8ebb1abac14019f59a04578ef3d0
Yang Chuanzhang <json@riemann.local>
2013-04-20 23:13:45 +0100

    Fix build script (add functionality to copy subdirectories)

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mkdir ./build
 mkdir ./build/assets
 
 # Copy files from assets to build, compile to .js if it's a .coffee file
-for f in ./assets/*; do
+for f in ./assets/*.*; do
   if echo "$f" | grep -qE ".coffee$"; then
     echo "Compiling $f"
     coffee -o ./build/assets/ -c $f
@@ -13,6 +13,12 @@ for f in ./assets/*; do
     echo "Copying $f"
     cp $f ./build/assets/
   fi
+done
+
+# Copy assets subdirs (these directories contain nothing that needs to be compiled
+for d in $(find ./assets/* -type d); do 
+  echo "Copying $d"; 
+  cp -R $d ./build/assets/
 done
 
 # Compile haml files
