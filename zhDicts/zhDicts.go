72507175a722505b6eb83c17838eb99421fee9fb
Jan Soendermann <jan.soendermann@googlemail.com>
2014-04-25 01:00:13 +0100

    Get db connection details from env vars

--- zhDicts/zhDicts.go
+++ zhDicts/zhDicts.go
@@ -1,6 +1,7 @@
 package zhDicts
 
 import (
+    "os"
     "database/sql"
     _ "github.com/lib/pq"
 )
@@ -9,7 +10,11 @@ var db *sql.DB
 
 func LoadDb() error {
     var err error
-    db, err = sql.Open("postgres", "user=json dbname=json host=localhost sslmode=disable")
+    dbname := os.Getenv("BMT_DB")
+    dbuser := os.Getenv("BMT_USER")
+    dbpw := os.Getenv("BMT_PW")
+
+	db, err = sql.Open("postgres", "user=" + dbuser + " dbname=" + dbname + " password=" + dbpw + " host=localhost sslmode=disable")
     if err != nil {
         return err
     }
